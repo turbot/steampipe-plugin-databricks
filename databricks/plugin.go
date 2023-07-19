1d2f72af263f407beb9a8c39a5ed8aa7a4ae7022
Karan Popat <karan@turbot.com>
2023-07-19 18:35:00 +0530

    Add table databricks_sharing_recipient

--- databricks/plugin.go
+++ databricks/plugin.go
@@ -54,6 +54,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"databricks_pipelines_pipeline_update":  tableDatabricksPipelinesPipelineUpdate(ctx),
 			"databricks_settings_ip_access_list":    tableDatabricksSettingsIpAccessList(ctx),
 			"databricks_sharing_provider":           tableDatabricksSharingProvider(ctx),
+			"databricks_sharing_recipient":          tableDatabricksSharingRecipient(ctx),
 			"databricks_sql_alert":                  tableDatabricksSQLAlert(ctx),
 			"databricks_sql_dashboard":              tableDatabricksSQLDashboard(ctx),
 			"databricks_sql_data_source":            tableDatabricksSQLDataSource(ctx),
